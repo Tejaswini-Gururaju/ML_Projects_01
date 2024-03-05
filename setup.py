bc5233014d4c777b8e7b0f37ac6036db1a704bd8
Tejaswini-Gururaju <tejaswiniacharya06@gmail.com>
2024-03-05 21:45:46 +0530

    Data Ingestion

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_requirements(file_path:str)->List[str]:
     '''
     requirements=[]
     with open(file_path) as file_obj:
-        requiremsnts=file_obj.readlines()
+        requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements ]
 
         if hypen_e_dot in requirements:
